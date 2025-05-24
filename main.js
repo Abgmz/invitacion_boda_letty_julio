5954874a15dea06bc4be2fdd59b1cfb468d6777d
Abraham Gomez <abraham.nava10@gmail.com>
2025-05-23 21:00:15 -0600

    Añadiendo funcionalidad de mapas y botones de confirmacion

--- main.js
+++ main.js
@@ -27,11 +27,68 @@
   }, 1000);
 })();
 
+//Implementacion del modal
 (() => {
   const modalEl = document.getElementById("wedding-modal");
   const modalBtn = document.getElementById("enter-site");
 
   modalBtn.addEventListener("click", (ev) => {
     modalEl.classList.add("hide-modal");
+  });
+})();
+
+// Implementacion de ruto para el lugar del evento
+(() => {
+  document
+    .querySelector(".reception_location-button")
+    .addEventListener("click", () => {
+      // Coordenadas del lugar (reemplázalas por las de tu sede)
+      const lat = 25.683103019029296;
+      const lng = -100.22113769127941;
+
+      // Detectar si es móvil para abrir app nativa
+      const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+      if (isMobile) {
+        // Abrir en apps nativas (Google Maps o Apple Maps)
+        window.open(
+          `https://www.google.com/maps/dir/?api=1&destination=${lat},${lng}&travelmode=driving`
+        );
+      } else {
+        // Abrir en web para desktop
+        window.open(`https://www.google.com/maps?q=${lat},${lng}`);
+      }
+    });
+})();
+
+//Implementacion invitados y confirmacion de asistencia
+(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const guestName = urlParams.get("invitado") || "Invitado Especial";
+  const guestNumberAccess = urlParams.get("acceso") || "1";
+  const numeroNovio = "528123855233";
+  const numeroNovia = "528116288544";
+  const mensaje = `¡Hola! Soy ${decodeURIComponent(
+    guestName
+  )}. Confirmo mi asistencia.`;
+
+  function enviarWhatsApp(numero) {
+    const mensajeCodificado = encodeURIComponent(mensaje);
+    window.open(`https://wa.me/${numero}?text=${mensajeCodificado}`, "_blank");
+  }
+
+  document.querySelector(".guest_card-name").textContent = guestName;
+  document.querySelector(
+    ".guest_card-resume em"
+  ).textContent = `${guestNumberAccess} ${
+    guestNumberAccess > 1 ? "accesos" : "acceso"
+  }`;
+
+  document.getElementById("confirmLetty").addEventListener("click", () => {
+    enviarWhatsApp(numeroNovia);
+  });
+
+  document.getElementById("confirmJulio").addEventListener("click", () => {
+    enviarWhatsApp(numeroNovio);
   });
 })();
